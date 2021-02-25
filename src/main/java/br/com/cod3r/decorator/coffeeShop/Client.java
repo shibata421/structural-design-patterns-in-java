33779ce42fa564b28d90a12723340f9f66179084
Fernando Shibata <shibata.421@gmail.com>
2021-02-25 15:13:16 -0300

    Implements coffeShop example of Decorator

--- src/main/java/br/com/cod3r/decorator/coffeeShop/Client.java
+++ src/main/java/br/com/cod3r/decorator/coffeeShop/Client.java
@@ -3,6 +3,8 @@ package br.com.cod3r.decorator.coffeeShop;
 import br.com.cod3r.decorator.coffeeShop.model.Drink;
 import br.com.cod3r.decorator.coffeeShop.model.Expresso;
 import br.com.cod3r.decorator.coffeeShop.model.Tea;
+import br.com.cod3r.decorator.coffeeShop.model.decorators.DoubleDrink;
+import br.com.cod3r.decorator.coffeeShop.model.decorators.Milk;
 
 public class Client {
 	
@@ -16,6 +18,7 @@ public class Client {
 	public static void main(String[] args) {
 		order("Expresso", new Expresso());
 		order("Tea", new Tea());
-		
+		order("Cafe au Lait", new Milk(new Expresso()));
+		order("Cafe au Lait Lungo", new DoubleDrink(new Milk(new Expresso())));
 	}
 }
--- src/main/java/br/com/cod3r/decorator/coffeeShop/model/decorators/DrinkDecorator.java
+++ src/main/java/br/com/cod3r/decorator/coffeeShop/model/decorators/DrinkDecorator.java
@@ -4,4 +4,10 @@ import br.com.cod3r.decorator.coffeeShop.model.Drink;
 
 public abstract class DrinkDecorator implements Drink {
 	
+	protected Drink drink;
+	
+	public DrinkDecorator(Drink drink) {
+		this.drink = drink;
+	}
+	
 }
